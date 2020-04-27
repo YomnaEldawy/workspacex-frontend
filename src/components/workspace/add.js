b9c312be25288c9b72686c098b8d180a6b6af0f3
yomna <yomna.eldawy@bosta.co>
2020-04-27 07:56:18 +0200

    map works

--- src/components/workspace/add.js
+++ src/components/workspace/add.js
@@ -1,123 +1,227 @@
 import React from "react";
-import loginImg from "../../login.svg";
-import { Alert } from "react-bootstrap";
-import Map from "./map";
+import { compose, withProps, lifecycle } from "recompose";
 
+import {
+  withScriptjs,
+  withGoogleMap,
+  GoogleMap,
+  Marker,
+} from "react-google-maps";
 const axios = require("axios");
-export default class Add_workspace extends React.Component {
+
+global.marker_position = null;
+const MyMapComponent = compose(
+  withProps({
+    googleMapURL:
+      "https://maps.googleapis.com/maps/api/js?key=AIzaSyAvcDy5ZYc2ujCS6TTtI3RYX5QmuoV8Ffw&libraries=geometry,drawing,places&key=AIzaSyA-xkiEOSnop65ZHIfEJD3GI9ZVC3-7qrQ&key=AIzaSyA-xkiEOSnop65ZHIfEJD3GI9ZVC3-7qrQ",
+    loadingElement: <div style={{ height: `100%` }} />,
+    containerElement: <div style={{ height: `400px`, width: "40%" }} />,
+    mapElement: <div style={{ height: `100%` }} />,
+  }),
+  lifecycle({
+    componentWillMount() {
+      const refs = {};
+
+      this.setState({
+        position: null,
+        onMarkerMounted: (ref) => {
+          refs.marker = ref;
+        },
+
+        onPositionChanged: () => {
+          const position = refs.marker.getPosition();
+          global.marker_position = refs.marker.getPosition();
+          console.log(position.toString());
+        },
+      });
+    },
+  }),
+  withScriptjs,
+  withGoogleMap
+)((props) => (
+  <GoogleMap defaultZoom={8} defaultCenter={{ lat: 26.820553, lng: 30.802498 }}>
+    {props.isMarkerShown && (
+      <Marker
+        position={{ lat: 26.820553, lng: 30.802498 }}
+        draggable={true}
+        ref={props.onMarkerMounted}
+        onPositionChanged={props.onPositionChanged}
+      />
+    )}
+  </GoogleMap>
+));
+
+class MyParentComponentWrapper extends React.PureComponent {
   state = {
-    loginDetails: "",
-    message: "",
+    isMarkerShown: false,
+    ws_name: "ws_name",
+    city: "city",
+    phone: "phone",
+    streetName: "streetName",
+    streetNumber: "streetNumber",
+    latitude: "",
+    longitude: "",
+    opensAt: "",
+    closesAt: "",
+    day_to_open: "",
+    day_to_close: "",
   };
+
   addWorkspaceHandler = (e) => {
-    e.preventDefault();
-    console.log(this.props.location.state);
-    this.setState({ loginDetails: this.props.location.state });
-    axios({
-      method: "POST",
-      url: "http://localhost:5000/workspace/new",
-      data: { ...this.state, loginDetails: this.props.location.state },
-    }).then((response) => {
-      if (response.data.affectedRows) {
-        this.setState({
-          message: (
-            <Alert style={{ margin: "0.8rem" }} variant="success">
-              {"Added Successfully"}
-            </Alert>
-          ),
-        });
-        this.props.history.push({
-          pathname: "/login/",
-        });
-      } else {
-        this.setState({
-          message: (
-            <Alert style={{ margin: "0.8rem" }} variant="danger">
-              {"Error adding workspace"}
-            </Alert>
-          ),
-        });
+    this.setState(
+      {
+        latitude: global.marker_position.lat(),
+        longitude: global.marker_position.lng(),
+      },
+      function () {
+        e.preventDefault();
+        axios
+          .post("http://localhost:5000/workspace/new", this.state)
+          .then((response) => {
+            console.log(response.data);
+          });
       }
-    });
+    );
   };
 
   changeStateHandler = (e) => {
     this.setState({ [e.target.name]: e.target.value });
   };
-
   render() {
     return (
-      <div className="add workspace">
-        <div className="container">
-          <div>{this.state.message}</div>
-          <div className="base-container" ref={this.props.containerRef}>
-            {<Map />}
-            <div className="header">New Workspace</div>
-            <div className="content">
-              <div className="image">
-                <img src={loginImg} />
-              </div>
-              <div className="form">
-                <div className="form-group">
-                  <label htmlFor="ws_name">Workspace name</label>
-                  <input
-                    onChange={this.changeStateHandler}
-                    type="text"
-                    name="ws_name"
-                    placeholder="workspace name"
-                  />
-                </div>
-                <div className="form-group">
-                  <label htmlFor="city">City</label>
-                  <input
-                    onChange={this.changeStateHandler}
-                    type="text"
-                    name="city"
-                    placeholder="city"
-                  />
-                </div>
-                <div className="form-group">
-                  <label htmlFor="password">Phone</label>
-                  <input
-                    onChange={this.changeStateHandler}
-                    type="text"
-                    name="phone"
-                    placeholder="phone"
-                  />
-                </div>
-                <div className="form-group">
-                  <label>Street Name</label>
-
-                  <input
-                    onChange={this.changeStateHandler}
-                    type="text"
-                    name="streetName"
-                    placeholder="street name"
-                  />
-                </div>
-                <div className="form-group">
-                  <label>Street Number</label>
-                  <input
-                    onChange={this.changeStateHandler}
-                    type="text"
-                    name="streetNumber"
-                    placeholder="street number"
-                  />
-                </div>
-              </div>
-            </div>
-            <div className="footer">
-              <button
-                onClick={this.addWorkspaceHandler}
-                type="button"
-                className="btn"
-              >
-                Add
-              </button>
-            </div>
-          </div>
+      <div>
+        <MyMapComponent isMarkerShown={true} />
+        <form>
+          <form>
+            <label>
+              Warkspace name:
+              <form>
+                <input
+                  type="text"
+                  name="ws_name"
+                  onChange={this.changeStateHandler}
+                  placeholder="workspace name"
+                />
+              </form>
+            </label>
+          </form>
+          <form>
+            <label>
+              Phone:
+              <form>
+                <input
+                  type="text"
+                  name="phone"
+                  onChange={this.changeStateHandler}
+                  placeholder="01xxxxxxxxx"
+                />
+              </form>
+            </label>
+          </form>
+          <form>
+            <label>
+              City:
+              <form>
+                <input
+                  type="text"
+                  name="city"
+                  onChange={this.changeStateHandler}
+                  placeholder="city"
+                />
+              </form>
+            </label>
+          </form>
+          <form>
+            <label>
+              Street name:
+              <form>
+                <input
+                  type="text"
+                  name="streetName"
+                  onChange={this.changeStateHandler}
+                  placeholder="street name"
+                />
+              </form>
+            </label>
+          </form>
+          <form>
+            <label>
+              Street number:
+              <form>
+                <input
+                  type="text"
+                  name="streetNumber"
+                  onChange={this.changeStateHandler}
+                  placeholder="street number"
+                />
+              </form>
+            </label>
+          </form>
+          <form>
+            <label>
+              Opens at:
+              <form>
+                <input
+                  type="text"
+                  name="opensAt"
+                  onChange={this.changeStateHandler}
+                  placeholder="hh:mm:ss"
+                />
+              </form>
+            </label>
+          </form>
+          <form>
+            <label>
+              Closes at:
+              <form>
+                <input
+                  type="text"
+                  name="closesAt"
+                  onChange={this.changeStateHandler}
+                  placeholder="hh:mm:ss"
+                />
+              </form>
+            </label>
+          </form>
+          <form>
+            <label>
+              Day to open:
+              <form>
+                <input
+                  type="text"
+                  name="Day_to_open"
+                  onChange={this.changeStateHandler}
+                  placeholder="ex: Sunday,Monday..etc"
+                />
+              </form>
+            </label>
+          </form>
+          <form>
+            <label>
+              Day to close:
+              <form>
+                <input
+                  type="text"
+                  name="Day_to_close"
+                  onChange={this.changeStateHandler}
+                  placeholder="ex: Sunday,Monday..etc"
+                />
+              </form>
+            </label>
+          </form>
+        </form>
+        <div className="footer">
+          <button
+            onClick={this.addWorkspaceHandler}
+            type="button"
+            className="btn"
+          >
+            Add
+          </button>
         </div>
       </div>
     );
   }
 }
+
+export default MyParentComponentWrapper;
